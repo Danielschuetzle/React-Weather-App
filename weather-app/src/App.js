79c47afe04a8c1f42674ef7b472d0f75064a8bef
Daniel Schützle <danielschuetzle@gmail.com>
2023-05-25 16:34:16 +0200

    styling added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,64 +1,83 @@
 import React, { useState, useEffect } from "react";
 import List from "./components/List";
+import Form from "./components/Form";
 
 function App() {
+  // State for activities
   const [activities, setActivities] = useState(
     JSON.parse(localStorage.getItem("activities")) || []
   );
-  const [weather, setWeather] = useState(null);
 
+  // State for weather
+  const [weather, setWeather] = useState({});
+
+  // State for showing only good weather activities
+  const [showOnlyGoodWeather, setShowOnlyGoodWeather] = useState(false);
+
+  // Function to fetch weather data
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather/europe"
+      );
+      const data = await response.json();
+      setWeather(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  // Fetch weather data on initial render
   useEffect(() => {
-    const fetchWeather = async () => {
-      try {
-        const response = await fetch(
-          "https://example-apis.vercel.app/api/weather/europe"
-        );
-        const data = await response.json();
-        setWeather(data);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
     fetchWeather();
   }, []);
 
+  // Update local storage when activities state changes
   useEffect(() => {
     localStorage.setItem("activities", JSON.stringify(activities));
   }, [activities]);
 
+  // Function to handle adding a new activity
   const handleAddActivity = (newActivity) => {
     const activityWithId = { ...newActivity, id: Date.now() };
-    setActivities((prevActivities) => [...prevActivities, activityWithId]);
+    setActivities([...activities, activityWithId]);
   };
 
+  // Function to handle toggling of showing only good weather activities
+  const handleToggleGoodWeather = () => {
+    setShowOnlyGoodWeather(
+      (prevShowOnlyGoodWeather) => !prevShowOnlyGoodWeather
+    );
+  };
+
+  // Function to handle deleting an activity
   const handleDeleteActivity = (id) => {
     setActivities((prevActivities) =>
       prevActivities.filter((activity) => activity.id !== id)
     );
   };
 
-  if (!weather) {
-    return <div>Loading...</div>;
-  }
-
-  const isGoodWeather =
-    weather.condition === "clear" || weather.condition === "partly-cloudy";
-
-  const filteredActivities = activities.filter(
-    (activity) => activity.isForGoodWeather === isGoodWeather
-  );
-
   return (
     <div className="App">
+      <h1>Weather & Activities App</h1>
+
+      {/* Display weather condition and temperature */}
       <h2>
-        {weather.condition} {weather.temperature}°C {weather.isGoodWeather}
+        {weather.condition} {weather.temperature}°C
       </h2>
-      <List
+
+      {/* Add Activity Form */}
+      <Form
         onAddActivity={handleAddActivity}
-        activities={filteredActivities}
-        isGoodWeather={isGoodWeather}
+        onToggleGoodWeather={handleToggleGoodWeather}
+      />
+
+      {/* List of activities */}
+      <List
+        activities={activities}
         onDeleteActivity={handleDeleteActivity}
+        isGoodWeather={weather.isGoodWeather}
+        showOnlyGoodWeather={showOnlyGoodWeather}
       />
     </div>
   );
--- weather-app/src/components/List.js
+++ weather-app/src/components/List.js
@@ -1,22 +1,66 @@
 import React from "react";
-import ActivityForm from "./Form";
 
-const List = ({ onAddActivity, activities, onDeleteActivity }) => {
+const List = ({
+  activities,
+  onDeleteActivity,
+  isGoodWeather,
+  showOnlyGoodWeather,
+}) => {
+  // Filter activities based on showOnlyGoodWeather and isGoodWeather conditions
+  const filteredActivities = showOnlyGoodWeather
+    ? activities.filter(
+        (activity) => activity.isForGoodWeather && isGoodWeather
+      )
+    : activities;
+
   return (
     <div>
-      <h2>Weather & Activities App</h2>
+      <h2>Activities for {isGoodWeather ? "Good" : "Bad"} Weather</h2>
 
-      <ActivityForm onAddActivity={onAddActivity} />
-
-      <ul>
-        {activities.map((activity) => (
-          <li key={activity.id}>
-            {activity.name}
-            <button onClick={() => onDeleteActivity(activity.id)}>x</button>
-          </li>
-        ))}
-      </ul>
+      {/* Card container */}
+      <div
+        style={{
+          backgroundColor: "#fff",
+          padding: "20px",
+          borderRadius: "5px",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <ul
+          style={{
+            listStyle: "none",
+            padding: 0,
+            margin: 0,
+          }}
+        >
+          {/* Map through filtered activities and render list items */}
+          {filteredActivities.map((activity) => (
+            <li
+              key={activity.id}
+              style={{
+                backgroundColor: activity.isForGoodWeather
+                  ? "orange"
+                  : "lightblue",
+                padding: "10px",
+                borderRadius: "5px",
+                marginBottom: "10px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <span>{activity.name}</span>
+              </div>
+              <button onClick={() => onDeleteActivity(activity.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
+
 export default List;

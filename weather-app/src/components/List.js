27d3239bce099b129d4bcc2ccfc1757c8f99025e
Daniel Schützle <danielschuetzle@gmail.com>
2023-05-25 16:44:29 +0200

    styling list added

--- weather-app/src/components/List.js
+++ weather-app/src/components/List.js
@@ -1,46 +1,54 @@
 import React from "react";
 
-const List = ({
-  activities,
-  onDeleteActivity,
-  isGoodWeather,
-  showOnlyGoodWeather,
-}) => {
-  // Filter activities based on showOnlyGoodWeather and isGoodWeather conditions
-  const filteredActivities = showOnlyGoodWeather
-    ? activities.filter(
-        (activity) => activity.isForGoodWeather && isGoodWeather
-      )
-    : activities;
+const List = ({ activities, onDeleteActivity, isGoodWeather }) => {
+  const goodWeatherActivities = activities.filter(
+    (activity) => activity.isForGoodWeather
+  );
+  const badWeatherActivities = activities.filter(
+    (activity) => !activity.isForGoodWeather
+  );
 
   return (
     <div>
-      <h2>Activities for {isGoodWeather ? "Good" : "Bad"} Weather</h2>
+      <h2>Activities List</h2>
 
-      {/* Card container */}
-      <div
-        style={{
-          backgroundColor: "#fff",
-          padding: "20px",
-          borderRadius: "5px",
-          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <ul
-          style={{
-            listStyle: "none",
-            padding: 0,
-            margin: 0,
-          }}
-        >
-          {/* Map through filtered activities and render list items */}
-          {filteredActivities.map((activity) => (
+      {/* Good Weather Activities */}
+      <div>
+        <h3>Good Weather Activities</h3>
+        <ul>
+          {goodWeatherActivities.map((activity) => (
             <li
               key={activity.id}
               style={{
-                backgroundColor: activity.isForGoodWeather
-                  ? "orange"
-                  : "lightblue",
+                backgroundColor: "orange",
+                padding: "10px",
+                borderRadius: "5px",
+                marginBottom: "10px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <span>{activity.name}</span>
+              </div>
+              <button onClick={() => onDeleteActivity(activity.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Bad Weather Activities */}
+      <div>
+        <h3>Bad Weather Activities</h3>
+        <ul>
+          {badWeatherActivities.map((activity) => (
+            <li
+              key={activity.id}
+              style={{
+                backgroundColor: "lightblue",
                 padding: "10px",
                 borderRadius: "5px",
                 marginBottom: "10px",
